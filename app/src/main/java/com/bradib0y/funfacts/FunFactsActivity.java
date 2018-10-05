f4eaf4268cfb796124a53e8d76ecfdecbd7997dc
Balazs Pukli <balazsptm@gmail.com>
2018-10-05 04:25:32 +0200

    data

--- app/src/main/java/com/bradib0y/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/bradib0y/funfacts/FunFactsActivity.java
@@ -6,10 +6,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class FunFactsActivity extends AppCompatActivity {
 
     private TextView factTextView;
     private Button showFactButton;
+
+    String[] facts = {
+            "Ants stretch when they wake up in the morning.",
+            "Ostriches can run faster than horses.",
+            "Olympic gold medals are actually made mostly of silver.",
+            "You are born with 300 bones; by the time you are an adult you will have 206.",
+            "It takes about 8 minutes for light from the Sun to reach Earth.",
+            "Some bamboo plants can grow almost a meter in just one day.",
+            "The state of Florida is bigger than England.",
+            "Some penguins can leap 2-3 meters out of the water.",
+            "On average, it takes 66 days to form a new habit.",
+            "Mammoths still walked the earth when the Great Pyramid was being built." };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +36,9 @@ public class FunFactsActivity extends AppCompatActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String fact = "Ostriches can run faster than horses.";
+                Random r = new Random();
+                int randomNumber = r.nextInt(facts.length);
+                String fact = facts[randomNumber];
                 factTextView.setText(fact);
             }
         };
