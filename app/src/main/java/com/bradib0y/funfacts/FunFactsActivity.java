c7f3c11feef4ef24886466c0a66b4ea2260c4559
Balazs Pukli <balazsptm@gmail.com>
2018-10-05 04:51:45 +0200

    added colorwheel

--- app/src/main/java/com/bradib0y/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/bradib0y/funfacts/FunFactsActivity.java
@@ -1,9 +1,12 @@
 package com.bradib0y.funfacts;
 
+import android.graphics.Color;
+import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -12,8 +15,10 @@ public class FunFactsActivity extends AppCompatActivity {
 
     private TextView factTextView;
     private Button showFactButton;
+    private RelativeLayout relativeLayout;
 
     private FactBook factBook = new FactBook();
+    private ColorWheel colorWheel = new ColorWheel();
 
 
 
@@ -24,12 +29,14 @@ public class FunFactsActivity extends AppCompatActivity {
 
         factTextView = findViewById(R.id.factTextView);
         showFactButton = findViewById(R.id.showTextButton);
+        relativeLayout = findViewById(R.id.funFactsLayout);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 factTextView.setText(factBook.get());
+                relativeLayout.setBackgroundColor(Color.parseColor(colorWheel.get()));
             }
         };
 
